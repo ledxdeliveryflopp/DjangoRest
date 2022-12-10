d786a4322ca115e6b2a0c9f8dae04d026db32c29
shalyapinalexander@gmail.com <ashaliapin@dnomads.pro>
2022-12-10 23:42:00 +0700

    Добавил связи m2m поправил вывод информации

--- book/models.py
+++ book/models.py
@@ -12,6 +12,7 @@ class Book(Core):
     genre = models.ManyToManyField('Genre', verbose_name='Жанр', blank=True)
     category = models.ManyToManyField('Category', verbose_name='Категория', blank=True)
     publisher = models.ManyToManyField('Publisher', verbose_name='Издательство', blank=True)
+    lang = models.ManyToManyField('Languages', verbose_name='Язык книги', blank=True)
 
     # photoPreview = models.ImageField(validators=[validate_image], upload_to='cover',
     #                                   verbose_name='Изображения',
@@ -23,6 +24,10 @@ class Book(Core):
         unique_together = ('title', 'author', 'year_of_rel')
         verbose_name = 'Книга'
         verbose_name_plural = 'Книги'
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        return
 
 
 class Genre(Core):
@@ -44,3 +49,10 @@ class Publisher(Core):
     class Meta:
         verbose_name = 'Издательство'
         verbose_name_plural = 'Издательство'
+
+
+class Languages(Core):
+    """ Модель языка """
+    class Meta:
+        verbose_name = 'Язык книги'
+        verbose_name_plural = 'Язык книги'
--- book/serializer.py
+++ book/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
-from .models import Book, Genre, Category
+from .models import Book, Genre, Category, Publisher, Languages
 
 
 class GenreSerializer(ModelSerializer):
@@ -15,15 +15,29 @@ class CategorySerializer(ModelSerializer):
         fields = '__all__'
 
 
+class PublisherSerializer(ModelSerializer):
+    class Meta:
+        model = Publisher
+        fields = '__all__'
+
+
+class LanguagesSerializer(ModelSerializer):
+    class Meta:
+        model = Languages
+        fields = '__all__'
+
+
 class BooksSerializer(ModelSerializer):
     """ """
     author = SerializerMethodField(method_name='get_author')
     genre = GenreSerializer(many=True)
     category = CategorySerializer(many=True)
+    publisher = PublisherSerializer(many=True)
+    lang = LanguagesSerializer(many=True)
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'year_of_rel', 'genre', 'category', 'publisher', 'author']
+        fields = ['id', 'title', 'year_of_rel', 'genre', 'category', 'publisher', 'lang', 'author']
 
     @staticmethod
     def get_author(obj) -> str:
